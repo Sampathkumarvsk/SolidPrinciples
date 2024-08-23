62dde02ca3db75eca7be492c9327e45359f4eca1
Sampath <sampathkumar.vemulakonda@gmail.com>
2024-08-23 10:19:06 +0530

    New Files

---
+++ src/JavaPrograms/RemoveTrailingZeroes.java
@@ -0,0 +1,22 @@
+package JavaPrograms;
+
+public class RemoveTrailingZeroes {
+
+	public static void main(String[] args) {
+		
+		String trailingZeroes = "4010000000";
+		
+		for(int i=trailingZeroes.length()-1; i>0 ;i--)
+		{
+			if (trailingZeroes.charAt(i)!='0')
+			{
+			  System.out.println(trailingZeroes.substring(0, i+1));
+			  break;
+			}
+		}
+		
+		
+		
+	}
+
+}
